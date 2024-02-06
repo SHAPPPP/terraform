74e1c9a3471325d7bc368e4bb730c5d11b7f6aa1
SHAPPPP <annmw@naver.com>
2024-02-06 11:28:37 +0900

    2024-02-06

---
+++ 00_test/main.tf
@@ -0,0 +1,29 @@
+terraform {
+	# Terraform 버전 지정
+    required_version = ">= 1.0.0, < 2.0.0"
+	# 공급자 지정
+    required_providers {
+        aws = {
+    	    source = "hashicorp/aws"
+	    version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-09eb4311cbaecf89d"
+    instance_type = "t2.micro"
+    key_name = "aws12-key"
+    
+    tags = {
+	Name =  "aws12-example"
+	org = "busanit"
+    }
+}
+
+
+
